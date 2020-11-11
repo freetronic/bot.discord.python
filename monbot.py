33c2d878c5169c58a3e2e632a6fbd870233cad4d
djsyl <f8byc@hotmail.com>
2020-11-11 13:00:18 +0100

    Rédaction en cours .......................

--- monbot.py
+++ monbot.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 #coding:utf8
-#rédaction en cours .......................
+#Rédaction en cours ..........................
 import discord #attention a ne pas nomer ce fichier discord.py
 
 
